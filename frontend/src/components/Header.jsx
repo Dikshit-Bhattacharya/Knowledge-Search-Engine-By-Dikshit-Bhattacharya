4f8f3c886251fcab442f4666d168b41f424198c8
Dikshit-Bhattacharya <dikshitbhattacharya@gmail.com>
2025-10-16 21:06:25 +0530

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Knowledge-base Search Engine</h1>
+      <p>Developed by- Dikshit Bhattacharya</p>
+    </header>
+  );
+}
+
+export default Header;
